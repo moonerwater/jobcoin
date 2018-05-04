f833ff2fb0eb263f83e16c52a4b9528301b4bbf8
zhou <zhou@163.com>
2018-05-04 10:44:15 +0800

    5.4 index building

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -9,7 +9,14 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        $this->view->disable();
-        echo '金职链还在开发中';
+        //$this->view->disable();
+        //echo '金职链还在开发中';
     }
+    
+	public function animationTestAction()
+    {
+        //$this->view->disable();
+        //echo '金职链还在开发中';
+    }
+
 }
