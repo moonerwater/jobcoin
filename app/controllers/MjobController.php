bbf04e00f49a90d417e1255b04646c31ae621af4
zhou <zhou@163.com>
2018-06-19 09:32:06 +0800

    Mjob 6.19 t1

---
+++ app/controllers/MjobController.php
@@ -0,0 +1,90 @@
+<?php
+
+class MjobController extends ControllerBase
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function indexAction()
+    {
+        
+    }
+
+    public function loginAction()
+    {
+        
+    }
+
+    public function mainAction()
+    {
+        
+    }
+
+    public function promoteAction()
+    {
+        
+    }
+    
+    public function recordAction()
+    {
+        
+    }
+
+    public function followwechatAction()
+    {
+        
+    }
+
+    public function inviteAction()
+    {
+        
+    }
+
+    public function thirdpartyAction()
+    {
+        
+    }
+
+    public function settingAction()
+    {
+        
+    }
+
+    public function warrantAction()
+    {
+        
+    }
+
+    public function identityAction()
+    {
+        
+    }
+
+    public function strategyAction()
+    {
+        
+    }
+
+    public function dataplateAction()
+    {
+        
+    }
+
+    public function assetsAction()
+    {
+        
+    }
+
+    public function iaeAction()
+    {
+        
+    }
+
+    public function invitecardAction()
+    {
+        
+    }
+
+}
