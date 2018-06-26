0bc42cd26186faad6629d804a44070eebf45c029
zhou <zhou@163.com>
2018-06-26 16:25:04 +0800

    t4 done

---
+++ app/library/Idcard.php
@@ -0,0 +1,70 @@
+<?php
+
+use Phalcon\Config\Adapter\Ini;
+
+class Idcard extends \Phalcon\Mvc\User\Component
+{
+
+    //构造方法初始化
+    public function __construct()
+    {
+
+    }
+
+    public function sendIdcard($imgbase64){
+        $smsConf = array(
+            'key'   => '5997331e2ed0a3d8079c83e8556b4475', //您申请的APPKEY
+            'image'    => $imgbase64,
+            'side'    => 'front'
+        );
+        $content = $this->juhecurl('http://apis.juhe.cn/idimage/verify', $smsConf, 1); //请求发送
+
+        if($content){
+            $result = json_decode($content,true);
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 请求接口返回内容
+     * @param  string $url [请求的URL地址]
+     * @param  string $params [请求的参数]
+     * @param  int $ipost [是否采用POST形式]
+     * @return  string
+     */
+    public function juhecurl($url,$params=false,$ispost=0){
+        $httpInfo = array();
+        $ch = curl_init();
+
+        curl_setopt( $ch, CURLOPT_HTTP_VERSION , CURL_HTTP_VERSION_1_1 );
+        curl_setopt( $ch, CURLOPT_USERAGENT , 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22' );
+        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT , 30 );
+        curl_setopt( $ch, CURLOPT_TIMEOUT , 30);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER , true );
+        if( $ispost )
+        {
+            curl_setopt( $ch , CURLOPT_POST , true );
+            curl_setopt( $ch , CURLOPT_POSTFIELDS , $params );
+            curl_setopt( $ch , CURLOPT_URL , $url );
+        }
+        else
+        {
+            if($params){
+                curl_setopt( $ch , CURLOPT_URL , $url.'?'.$params );
+            }else{
+                curl_setopt( $ch , CURLOPT_URL , $url);
+            }
+        }
+        $response = curl_exec( $ch );
+        if ($response === FALSE) {
+            //echo "cURL Error: " . curl_error($ch);
+            return false;
+        }
+        $httpCode = curl_getinfo( $ch , CURLINFO_HTTP_CODE );
+        $httpInfo = array_merge( $httpInfo , curl_getinfo( $ch ) );
+        curl_close( $ch );
+        return $response;
+    }
+}
