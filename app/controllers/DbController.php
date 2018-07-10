0ca48a671930e403ba501b816069fd96ddf5a5ec
zhou <zhou@163.com>
2018-07-10 17:03:44 +0800

    t2

---
+++ app/controllers/DbController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * DbController
+ *
+ * Manage errors
+ */
+class DbController extends ControllerH5
+{
+    public function indexAction()
+    {
+        
+    }
+
+    public function detailAction()
+    {
+
+    }
+
+    public function show401Action()
+    {
+
+    }
+
+    public function show500Action()
+    {
+
+    }
+}
