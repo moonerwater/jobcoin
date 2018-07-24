7add971916e6d5b9f5059b0c1bdd927e43506e15
mooner <moonerwater@outlook.com>
2018-07-24 11:00:28 +0800

    add

--- app/controllers/DbController.php
+++ app/controllers/DbController.php
@@ -12,9 +12,23 @@ class DbController extends ControllerH5
         parent::initialize();
     }
     
-    public function indexAction()
-    {
-        
+    public function indexAction() {
+        $this->checkNoUserGoLogin();
+        //
+        $userid = $this->userinfo['id'];
+
+        //
+        $list = \DbList::find(array('', 'order' => 'id desc'));
+        $list = $list->toArray();
+        foreach($list as $k => $v){
+            $product = \DbProduct::findFirstById($v['product_id']);
+            $list[$k]['name'] = $product->name;
+            $list[$k]['imgs'] = explode(',', $product->imgs);
+            $list[$k]['percent'] = (number_format($v['already_num']/$v['need_num']*100, 2));
+        }
+        $data['list'] = $list;
+        //
+        $this->view->setVar('data', $data);
     }
 
     public function detailAction()
