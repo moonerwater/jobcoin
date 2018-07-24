67a2b22e960342201eaf33edbab8d5d836735630
mooner <moonerwater@outlook.com>
2018-07-24 17:15:42 +0800

    add

--- app/controllers/ControllerH5.php
+++ app/controllers/ControllerH5.php
@@ -42,6 +42,30 @@ class ControllerH5 extends ControllerBase
         return $ip[0].'.***'.'.***.'.$ip[3];
     }
 
+    protected function getIp() {
+        if(!empty($_SERVER["HTTP_CLIENT_IP"]))
+        {
+            $cip = $_SERVER["HTTP_CLIENT_IP"];
+        }
+        else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+        {
+            $cip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }
+        else if(!empty($_SERVER["REMOTE_ADDR"]))
+        {
+            $cip = $_SERVER["REMOTE_ADDR"];
+        }
+        else
+        {
+            $cip = '';
+        }
+        preg_match("/[\d\.]{7,15}/", $cip, $cips);
+        $cip = isset($cips[0]) ? $cips[0] : 'unknown';
+        unset($cips);
+
+        return $cip;
+    }
+
     protected function checkUserGoMain(){
         if($this->userinfo){
             $this->response->redirect('mjob/main');
